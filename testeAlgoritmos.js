ea513262732cc6c1bba35af46d1cf1911556a38a
Chuks Ricardo Iloegbunam <48604291+ChuksII@users.noreply.github.com>
2020-09-17 15:57:17 -0300

    Alteração na resolução dos problemas do teste

--- testeAlgoritmos.js
+++ testeAlgoritmos.js
@@ -1,4 +1,11 @@
+function testeAlgoritmos( ){
+
 //Questão 1
+
+array_to_sort = [9,3,2,19,3,4,10,34,-99,99]
+console.log("Resposta da Questão 1: "+ sort(array_to_sort));
+
+
 function sort(array) {
     var done = false;
     while (!done) {
@@ -13,42 +20,64 @@ function sort(array) {
       }
     }
   
-    return auxArray;
+    return array;
   }
   
-  array_to_sort = [9,3,2,19,3,4,10,34,-99,99]
-  sort(array_to_sort);
-  console.log(array_to_sort);
-
 
 
 
 //Questão 2
-function factorial() {
-    var fac = Math.floor(Math.random() * 65536);
-    for(var i = 1; i < n; i++){
-        fac = fac * i;
+
+console.log("Resposta da Questão 2: " + fatorial(10));
+
+function fatorial(value) {
+  if(value < 0) {
+    return 'Valor não pode ser negativo';
+  } else if ( (value == 0) || (value == 1) ) {
+
+    return 1;
+  } else {
+
+    var fac = 1;
+    for(x = value; x > 1 ; x--) {
+      fac = fac * x;
     }
     return fac;
+  } 
 }
-console.log(fac);
-
 
 
 //Questão 3
-function fibonacci(){
-    var cont = 0;
-    var aux = 1, fibo = 0, temp;  
-    
-    while (cont >= 0){
-        temp = aux;
-        aux = aux + fibo;
-        fibo = temp;
-        cont++;
-    }
-    console.log(fibo);
+fibonnacci();
+function fibonnacci(){
+  var fibo = 1, aux = 0;
+  console.log("Resposta da Questão 3:")
+  console.log(aux);
+  console.log(fibo);
+
+
+  for(var i = 0; i < 8; i++){
+      fibo = fibo + aux;
+      aux = fibo - aux;
+      console.log(fibo)
+  }
+}
+
+//Questão 5
+var objeto1 = {x1:1, y1:1, x2:2, y2:2}
+var objeto2 = {x1:10, y1:10, x2:11, y2:11}
+console.log("Resposta da questão 5: " + colisao(objeto1, objeto2));
+
+function colisao(obj1, obj2) {
+
+  if(obj1.x1 === obj2.x1 || obj1.x2 === obj2.x2 
+    || obj1.y1 === obj2.y1 || obj1.y2 === obj2.y2) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 
-
-
+}
